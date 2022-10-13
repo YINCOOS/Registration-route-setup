882da5227d384271a67fa066d9c715d9a4eededd
Decagon <decagon@Decagons-MacBook-Air.local>
2022-10-13 04:51:47 +0100

    added biodata page

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import { register } from "./interfaces/register.interface";
+import { biodata } from "./interfaces/biodata.interface";
+
 
 dotenv.config();
 
@@ -11,16 +13,25 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.get('/', (req: Request, res: Response) => {
+    console.log(req.query)
   res.send('Express + TypeScript Server');
 });
 
 app.post(`${BASE_URL}/register`, (req: Request, res: Response) => {
   let data: register = req.body;
-  console.log(data);
+  console.log(data);//
   res.status(201).json({
     message: "User registered successfully",
   });
 });
+
+app.post(`${BASE_URL}/biodata`, (req: Request, res: Response) => {
+    let data: biodata = req.body;
+    console.log(data);//
+    res.status(201).json({
+      message: "User Bio_Data successfully Updated",
+    });
+  });
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
---
+++ interfaces/biodata.interface.ts
@@ -0,0 +1,12 @@
+export interface biodata {
+    firstName: string;
+    lastName: string;
+    email: string;
+    dob: string;
+    gender: string;
+    phone: string;
+    state: string;
+    city: string;
+    address: string;
+    pincode: string;
+}
